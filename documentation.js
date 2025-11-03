f06406b4fff995b466d1b05a78fc6eb3070fb893
olivierblanc42 <olivierhumanbooster@gmail.com>
2025-11-03 09:37:32 +0100

    add more documentation

--- documentation.js
+++ documentation.js
@@ -8,6 +8,19 @@ export const Documentation = {
             command: "aide",
             description: "Affiche la documentation du bot."
             
+        },
+        {
+            command: "calendar",
+            description: "Donne le lien du calendrier."
+
+        },
+        {
+            command: 'see-event',
+            description: 'voit les dernier evenement du calendrier',
+        },
+        {
+            command: 'event',
+            description: 'Ajout d\'un évènement',
         }
     ]
 
